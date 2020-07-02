427bad3e14c7ef902d9d2e9493a3b1346d3c899a
stefangeorgerobert <stefan_george_robert@yahoo.com>
2020-07-03 00:48:01 +0300

    startup options

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ export LOTUS_TOKEN="$(cat ~/.lotus/token)"
 echo $LOTUS_TOKEN
 mkdir ~/import
 mkdir ~/retrieve
-eval "node index.js &> ~/qab.log &"
+eval "node index.js --size 1000000 --standalone --cmdMode &> ~/qab.log &"
 eval "disown -r"
 eval "tail -f ~/qab.log"
 
